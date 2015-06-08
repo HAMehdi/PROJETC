f7b6cd36bdd2e025d39b0006e8ed8ac6640046a7
lpotages <lpotages@gmail.com>
2015-06-08 15:49:07 +0200

    Premier jet de la creation et destruction du cache

--- Cache_Project/cache.c
+++ Cache_Project/cache.c
@@ -5,14 +5,49 @@
 struct Cache *Cache_Create(const char *fic, unsigned nblocks, unsigned nrecords,
                            size_t recordsz, unsigned nderef)
 {
-	struct Cache cache = (struct Cache)malloc(sizeof(struct Cache));
+	struct Cache *cache = malloc(sizeof(struct Cache));
+	
+	cache->file = fic;
+	cache->fp = fopen(fic,"r+");
+	cache->nblocks = nblocks;
+	cache->nrecords = nrecords;
+	cache->recordsz = recordsz;
+	// TODO cache->blocksz = 0; 
+	cache->nderef = nderef;
+	// TODO cache->pstrategy = NULL;
+	
+	struct Cache_Instrument instrument = { 0, 0, 0, 0, 0 };
+	
+	cache->instrument = instrument;
+	
+	struct Cache_Block_Header* pfree = malloc(sizeof(struct Cache_Block_Header));
+	pfree->flags = VALID;
+	pfree->ibfile = 0;
+	pfree->ibcache = 0;
+	pfree->data = NULL;
+	
+	cache->pfree = pfree;
+	
+	struct Cache_Block_Header* headers = malloc(sizeof(struct Cache_Block_Header));
+	headers->flags = VALID;
+	headers->ibfile = 0;
+	headers->ibcache = 0;
+	headers->data = NULL;
+	
+	cache->headers = headers;
+	
 	return cache;
 }
 
 //! Fermeture (destruction) du cache.
 Cache_Error Cache_Close(struct Cache *pcache)
 {
-	return NULL;
+	free(cache->file);
+	free(cache->fp);
+	free(cache->pstrategy);
+	free(cache->pfree);
+	free(cache->headers);
+	free(cache);
 }
 
 //! Synchronisation du cache.
