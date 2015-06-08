aa30ce0b92f5941dc3e9d6628a35ef8767b6e8dc
lpotages <lpotages@gmail.com>
2015-06-08 11:27:49 +0200

    Initialisation des fichiers du Cache

---
+++ Cache_Project/cache.c
@@ -0,0 +1,45 @@
+#include "cache.h"
+
+//! Création du cache.
+struct Cache *Cache_Create(const char *fic, unsigned nblocks, unsigned nrecords,
+                           size_t recordsz, unsigned nderef)
+{
+	return NULL;
+}
+
+//! Fermeture (destruction) du cache.
+Cache_Error Cache_Close(struct Cache *pcache)
+{
+	return NULL;
+}
+
+//! Synchronisation du cache.
+Cache_Error Cache_Sync(struct Cache *pcache)
+{
+	return NULL;
+}
+
+//! Invalidation du cache.
+Cache_Error Cache_Invalidate(struct Cache *pcache)
+{
+	return NULL;
+}
+
+//! Lecture  (à travers le cache).
+Cache_Error Cache_Read(struct Cache *pcache, int irfile, void *precord)
+{
+	return NULL;
+}
+
+//! Écriture (à travers le cache).
+Cache_Error Cache_Write(struct Cache *pcache, int irfile, const void *precord)
+{
+	return NULL;
+}
+
+
+//! Résultat de l'instrumentation.
+struct Cache_Instrument *Cache_Get_Instrument(struct Cache *pcache)
+{
+	return NULL;
+}
---
+++ Cache_Project/low_cache.c
@@ -0,0 +1,7 @@
+#include "low_cache.c"
+
+//! Recherche d'un bloc libre.
+struct Cache_Block_Header *Get_Free_Block(struct Cache *pcache)
+{
+	return NULL;
+}
