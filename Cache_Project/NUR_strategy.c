ff6bed8ddabbc4ba5659633d3b65a25a70955205
Nathael Nogues <nogues.nathael@etu.unice.fr>
2015-06-08 11:00:30 +0200

    Copie RAND_strategy dans NUR_strategy

--- Cache_Project/NUR_strategy.c
+++ Cache_Project/NUR_strategy.c
@@ -1,6 +1,13 @@
-//
-// Created by user on 08/06/15.
-//
+/*!
+ * \file RAND_strategy.c
+ *
+ * \brief  Stratégie de remplacement au hasard..
+ * 
+ * \author Groupe 15
+ *
+ * NUR_strategy.c, 2015/06/08
+ */
+ 
 
 #include <assert.h>
 
@@ -8,3 +15,63 @@
 #include "low_cache.h"
 #include "random.h"
 #include "time.h"
+
+
+
+/*
+ * TODO (actuellement = RAND)
+ */ 
+void *Strategy_Create(struct Cache *pcache) 
+{
+    return NULL;
+}
+
+/*
+ * TODO
+ */ 
+void Strategy_Close(struct Cache *pcache)
+{
+}
+
+/*
+ * TODO
+ */ 
+void Strategy_Invalidate(struct Cache *pcache)
+{
+}
+
+/*
+ * TODO (actuellement = RAND)
+ */ 
+struct Cache_Block_Header *Strategy_Replace_Block(struct Cache *pcache) 
+{
+    int ib;
+    struct Cache_Block_Header *pbh;
+
+    /* On cherche d'abord un bloc invalide */
+    if ((pbh = Get_Free_Block(pcache)) != NULL) return pbh;
+
+    /* Sinon on tire un numéro de bloc au hasard */
+    ib = RANDOM(0, pcache->nblocks);
+    return &pcache->headers[ib];
+}
+
+
+/*
+ * TODO
+ */ 
+void Strategy_Read(struct Cache *pcache, struct Cache_Block_Header *pbh) 
+{
+}  
+
+/*
+ * TODO
+ */  
+void Strategy_Write(struct Cache *pcache, struct Cache_Block_Header *pbh)
+{
+} 
+
+char *Strategy_Name()
+{
+    return "NUR";
+}
