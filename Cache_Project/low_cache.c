7bbcac05afcbf1471363fbbaa976d465c8f437f6
lpotages <lpotages@gmail.com>
2015-06-08 12:44:50 +0200

    Completion de low_cache.c avec une premiere implementation

--- Cache_Project/low_cache.c
+++ Cache_Project/low_cache.c
@@ -1,7 +1,7 @@
-#include "low_cache.c"
+#include "low_cache.h"
 
 //! Recherche d'un bloc libre.
 struct Cache_Block_Header *Get_Free_Block(struct Cache *pcache)
 {
-	return NULL;
+	return *pcache->pfree;
 }
