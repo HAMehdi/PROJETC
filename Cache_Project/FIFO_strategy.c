93ffe6c014de58dcc5344da70805c53fb79fc225
Drym <drymfr@gmail.com>
2015-06-08 10:59:03 +0200

    prototype des fonctions

--- Cache_Project/FIFO_strategy.c
+++ Cache_Project/FIFO_strategy.c
@@ -1,6 +1,12 @@
-//
-// Created by user on 08/06/15.
-//
+/*!
+ * \file FIFO_strategy.c
+ *
+ * \brief  Stratégie de remplacement FIFO..
+ * 
+ * \author
+ *
+ * $Id: FIFO_strategy.c
+ */
 
 #include <assert.h>
 
@@ -8,3 +14,45 @@
 #include "low_cache.h"
 #include "random.h"
 #include "time.h"
+
+/*!
+ */
+void *Strategy_Create(struct Cache *pcache)
+{
+}
+
+/*!
+ */
+void Strategy_Close(struct Cache *pcache)
+{
+}
+
+/*!
+ */
+void Strategy_Invalidate(struct Cache *pcache)
+{
+}
+
+/*!
+ */
+struct Cache_Block_Header *Strategy_Replace_Block(struct Cache *pcache)
+{
+}
+
+
+/*!
+ */
+void Strategy_Read(struct Cache *pcache, struct Cache_Block_Header *pbh)
+{
+}
+
+/*!
+ */
+void Strategy_Write(struct Cache *pcache, struct Cache_Block_Header *pbh)
+{
+}
+
+char *Strategy_Name()
+{
+    return "FIFO";
+}
