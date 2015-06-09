66b04687e9ee4a3e3568ec1fe60f1ac5e8d68cee
lpotages <lpotages@gmail.com>
2015-06-09 12:40:12 +0200

    Correction du Get_Instrument de cache.c pour qu'il renvoie une copie et pas simplement un autre pointeur

--- Cache_Project/cache.c
+++ Cache_Project/cache.c
@@ -89,7 +89,12 @@ Cache_Error Cache_Write(struct Cache *pcache, int irfile, const void *precord)
 //! Résultat de l'instrumentation.
 struct Cache_Instrument *Cache_Get_Instrument(struct Cache *pcache)
 {
-	struct Cache_Instrument* nInstrument = &pcache->instrument;
+	struct Cache_Instrument* nInstrument = malloc(sizeof(struct Cache_Instrument));
+	nInstrument->n_reads = pcache->instrument.n_reads;
+	nInstrument->n_hits = pcache->instrument.n_hits;
+	nInstrument->n_deref = pcache->instrument.n_deref;
+	nInstrument->n_syncs = pcache->instrument.n_syncs;
+	nInstrument->n_writes = pcache->instrument.n_writes;
 	
 	pcache->instrument.n_reads = 0;
 	pcache->instrument.n_hits = 0;
