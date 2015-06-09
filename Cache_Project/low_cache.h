694acd2b77c5c3b09dd785c1f00b429d0e682791
Nathael Nogues <nogues.nathael@etu.unice.fr>
2015-06-09 10:49:26 +0200

    LowCache.h spécification du flag R pour NUR Strategy

--- Cache_Project/low_cache.h
+++ Cache_Project/low_cache.h
@@ -34,6 +34,7 @@
 typedef enum  {
     VALID = 0x1, //!< le bloc est valide
     MODIF = 0x2, //!< le bloc a été modifié
+    READ  = 0x3, //!< le bloc a été lu (NUR stat')
 } Cache_Flag;
 
 //! Entête de chaque bloc.
